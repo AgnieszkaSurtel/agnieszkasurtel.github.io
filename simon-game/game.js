b8cf2493feca2fd07a5501df9cbf018fb4ef870c
Agnieszka Surtel <surtel.aga@gmail.com>
2026-05-30 01:03:20 +0200

    correct display of the game restart message

--- simon-game/game.js
+++ simon-game/game.js
@@ -5,7 +5,11 @@ var started = false;
 var level = 0;
 
 
-$(document).keypress(function() {
+if (window.matchMedia("(max-width: 768px)").matches) {
+  $("#level-title").text("Tap Screen to Start");
+}
+
+$(document).keypress(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -17,67 +21,53 @@ document.addEventListener('touchstart', function f(ev) {
 
   if (started) return;
 
-
   if (ev.target.classList.contains("btn")) return;
 
-
   console.log("Wykryto zdarzenie typu: " + ev.type);
-
 
   var AudioContext = window.AudioContext || window.webkitAudioContext;
   if (AudioContext) {
     var context = new AudioContext();
   }
 
-  // Uruchomienie gry
   $("#level-title").text("Level " + level);
   nextSequence();
   started = true;
 });
 
-
-$(".btn").click(function() {
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
-
-
 function checkAnswer(currentLevel) {
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      
-     if (window.matchMedia("(max-width: 768px)").matches) {
-        $("#level-title").text("Game Over, Tap Screen to Restart");
-      } else {
-        $("#level-title").text("Game Over, Press Any Key to Restart");
-      }
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-      setTimeout(function () {
-        startOver();
-      }, 200); 
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
 
-
-    function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false; // Dopiero teraz gra pozwoli na kolejny start
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      $("#level-title").text("Game Over, Tap Screen to Restart");
+    } else {
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    setTimeout(function () {
+      startOver();
+    }, 200);
+  }
 }
 function nextSequence() {
   userClickedPattern = [];
